a53ffb7b3eb8fee4d45df9f58e8aa855f1962bd4
musamarabbani <usama.rabbani@invozone.com>
2022-06-27 19:29:26 +0500

    addUserstoGroup & removeUsersFromGroup

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -3,14 +3,17 @@ const {
 	accessChat,
 	fetchChat,
 	createGroupChat,
+	renameGroup,
+	addToGroup,
+	removeFromGroup,
 } = require('../controllers/chatController');
 const { auth } = require('../middlewares/authMiddleware');
 
 router.route('/').post(auth, accessChat);
 router.route('/').get(auth, fetchChat);
 router.route('/group').post(auth, createGroupChat);
-// router.route('/rename').post(auth, createGroupChat);
-// router.route('/groupRemove').post(auth, renameFromGroup);
-// router.route('/groupAdd').post(auth, addToGroup);
+router.route('/rename').put(auth, renameGroup);
+router.route('/groupAdd').put(auth, addToGroup);
+router.route('/groupRemove').put(auth, removeFromGroup);
 
 module.exports = router;

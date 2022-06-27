433f607a7db6fa902892779d3f40767721fb3b26
musamarabbani <usama.rabbani@invozone.com>
2022-06-27 19:04:13 +0500

    createGroup api

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,9 +1,14 @@
 const router = require('express').Router();
-const { accessChat, fetchChat } = require('../controllers/chatController');
+const {
+	accessChat,
+	fetchChat,
+	createGroupChat,
+} = require('../controllers/chatController');
 const { auth } = require('../middlewares/authMiddleware');
 
 router.route('/').post(auth, accessChat);
 router.route('/').get(auth, fetchChat);
+router.route('/group').post(auth, createGroupChat);
 // router.route('/rename').post(auth, createGroupChat);
 // router.route('/groupRemove').post(auth, renameFromGroup);
 // router.route('/groupAdd').post(auth, addToGroup);
